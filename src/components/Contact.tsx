9bf334da5da9c28904ec3e20b29654e539daed8c
Riaz-Hussain-Saifi <infosaifideveloper@gmail.com>
2025-02-20 00:15:31 +0600

    solve errors

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -44,7 +44,7 @@ export default function Contact() {
       } else {
         setSubmitStatus("error")
       }
-    } catch (error) {
+    } catch {
       setSubmitStatus("error")
     } finally {
       setIsSubmitting(false)
